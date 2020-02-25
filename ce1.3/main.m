2d39a6cd1742ffdd8098b0d8b18ca44e0e641976
jordi_puig <jordipuig37@gmail.com>
2020-02-25 11:46:30 +0100

    classe 25-02

--- ce1.3/main.m
+++ ce1.3/main.m
@@ -6,10 +6,11 @@ gg = @(x) 2 - 2 * sin(x);
 almax = 1; almin = 0.01; rho = 0.75;
 c1 = 0.1; c2 = 0.5;
 % initial point
-x = 10;
+x = 2.302774;
 % 0 = gradient method, 1 = newton method
-newt = 1;
+newt = 0c;
 
+fprintf('rk              Mk\n')
 [optima, it] = guoa(x, f, g, gg, almax, almin, c1, c2, rho, 10000, newt);
 
-fprintf("Mínim %d trobat en %d operacions des de %d\n", [optima, it, x])
+fprintf('Mínim %d trobat en %d operacions des de %d\n', [optima, it, x])
