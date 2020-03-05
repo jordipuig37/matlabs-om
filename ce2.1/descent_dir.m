c2480cdbfe9df4e6b7837dcd6f5183310655f61c
jordi_puig <jordipuig37@gmail.com>
2020-03-05 15:26:45 +0100

    mes metodes

--- ce2.1/descent_dir.m
+++ ce2.1/descent_dir.m
@@ -1,19 +1,29 @@
-function [d, b] = descent_dir(isd, x, x_1, f, g, h)
+function [d, b] = descent_dir(isd, irc, x, x_1, d_1, f, g, H, nu, k)
     b = 0;
     if isd == 1
+        % gradient method
         d = - g(x);
     elseif isd == 2
-        b = 0;
-        if icg == 1
-            b = (g(x)' * g(x)) / norm(g(x_1)); % FR
-        elseif icg == 2
-            b= (g(x)' * (g(x) - g(x_1))) / norm(g(x_1));
-            b = max(0, b); % PR+
+        % conjugate gradient (if not restart-> get beta, else beta=0)
+        if not(irc == 1 && mod(k, size(x, 2)) == 0) || not(irc == 2 && g(x).'* g(x_1) / norm(g(x)) >= nu)
+            if icg == 1
+                b = (g(x)' * g(x)) / norm(g(x_1)); % FR
+            elseif icg == 2
+                b= (g(x)' * (g(x) - g(x_1))) / norm(g(x_1));
+                b = max(0, b); % PR+
+            end
+        else
+            b = 0;
         end
         d = - g(x) + b * d_1; % trobem la nova direcció de descens
 
     elseif isd == 3
-        d = 0; % encara no sé
+        % quasi-newton
+        y = g(x) - g(x_1);
+        s = x - x_1;
+        p = 1 / (y.' * s);
+        H  = (I - p*s*y.') * H * (I - p*s*y.') +  p*s*y.';
+        d = - g(x)/H;
     else
         d = 0;
     end
--- ce2.1/om_uo_solve.m
+++ ce2.1/om_uo_solve.m
@@ -2,20 +2,21 @@
 function [xk,dk,alk,iWk,betak,Hk] = om_uo_solve(x,f,g,h,epsG,kmax,almax,almin,rho,c1,c2,iW,isd,icg,irc,nu)
     xk = [x]; dk = []; alk = [];
     iWk = []; betak = []; Hk = [h(x)];
-    k = 0;
+    k = 0; H =eye();
     d_1 = 0; x_1 = 0;
     while norm(g(x)) >= epsG && k < kmax
         % descent direction: %
-        [d, b] = descent_dir(isd, x, x_1, f, g, h);
+        [d, b, H] = descent_dir(isd, irc, x, x_1, f, g, H, k);
         d_1 = d; % actualitzem d_1 per la proxima iteració
         % line search: %
         if iW == 0
             al = 0; % formula
-            iwi = 3;
+            iWi = 3;
         else
             [al, iWi] = uo_BLS(x, d, f, g, almax, almin, rho, c1, c2, iW);
         end
         % computem el pas %
+        x_1 = x;
         x = x + al * d;
         % posem tota la informació al dia %
         xk = [xk, x]; dk = [dk, d]; alk = [alk, al];
